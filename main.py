7715bc36e40aaa9c286cab9f49b0759c48ce2dfa
ilyaBykonya <48153069+ilyaBykonya@users.noreply.github.com>
2024-02-04 01:58:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 if __name__ == '__main__':
     print('Git merge conflict resolve lab')
-    #
-    #
-    #
-    #
-    #
-    #
+    # ==========
+    print('_', end='')
+    # ==========
+    print('_', end='')
+    # ==========
+    print('_', end='')
+    # ==========
+    print('_', end='')
+    # ==========
+    print('_', end='')
+    # ==========
+    print('_', end='')
+    # ==========
