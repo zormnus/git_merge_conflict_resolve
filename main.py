fc036e4a2f5eee4edf5df1255b270f88fa4fe3dc
= <=>
2024-02-11 15:39:19 +0300

    Merge cheese pizza

--- main.py
+++ main.py
@@ -60,7 +60,6 @@ def print_pizza_to_ui(pizza: AbstractPizza) ->None:
 if __name__ == '__main__':
     pizza = MiddlePizza()
     pizza = Chicken(pizza)
-    pizza = Chicken(pizza)
-    pizza = Chicken(pizza)
+    pizza = Cheese(pizza)
     pizza = Chicken(pizza)
     print_pizza_to_ui(pizza)
